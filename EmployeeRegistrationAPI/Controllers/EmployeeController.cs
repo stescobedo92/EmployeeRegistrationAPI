2430c9a3b2593f066733ec372f99398eca73ad63
Sergio Triana <stescobedo.31@gmail.com>
2020-12-08 16:04:30 -0500

    Added the async SaveImage method

--- EmployeeRegistrationAPI/Controllers/EmployeeController.cs
+++ EmployeeRegistrationAPI/Controllers/EmployeeController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using EmployeeRegistrationAPI.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace EmployeeRegistrationAPI.Controllers
 {
@@ -14,10 +16,12 @@ namespace EmployeeRegistrationAPI.Controllers
     public class EmployeeController : ControllerBase
     {
         private readonly EmployeeDbContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public EmployeeController(EmployeeDbContext context)
+        public EmployeeController(EmployeeDbContext context,IWebHostEnvironment hostEnvironment)
         {
             _context = context;
+            this._hostEnvironment = hostEnvironment;
         }
 
         // GET: api/Employee
@@ -79,10 +83,11 @@ namespace EmployeeRegistrationAPI.Controllers
         [HttpPost]
         public async Task<ActionResult<EmployeeModel>> PostEmployeeModel([FromForm]EmployeeModel employeeModel)
         {
+            employeeModel.ImageName = await SaveImage(employeeModel.ImageFile);
             _context.Employees.Add(employeeModel);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetEmployeeModel", new { id = employeeModel.EmployeeID }, employeeModel);
+            return StatusCode(201);
         }
 
         // DELETE: api/Employee/5
@@ -104,6 +109,21 @@ namespace EmployeeRegistrationAPI.Controllers
         private bool EmployeeModelExists(int id)
         {
             return _context.Employees.Any(e => e.EmployeeID == id);
+        }
+
+        [NonAction]
+        public async Task<string> SaveImage(IFormFile imageFile)
+        {
+            string imageName = new String(Path.GetFileNameWithoutExtension(imageFile.FileName).Take(10).ToArray()).Replace(' ','-');
+            imageName = imageName + DateTime.Now.ToString("yymmssfff") + Path.GetExtension(imageFile.FileName);
+            var imagePath = Path.Combine(_hostEnvironment.ContentRootPath, "Images", imageName);
+
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            {
+                await imageFile.CopyToAsync(fileStream);
+            }
+
+            return imageName;
         }
     }
 }
