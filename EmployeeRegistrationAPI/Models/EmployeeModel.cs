19c9601b580cf7eea28f0158776ae1795a8a26d2
Sergio Triana <stescobedo.31@gmail.com>
2020-12-08 11:33:17 -0500

    Added the employee model class

---
+++ EmployeeRegistrationAPI/Models/EmployeeModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeRegistrationAPI.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        [Column(TypeName ="nvarchar(50)")]
+        public string EmployeeName { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Occupation { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string ImageName { get; set; }
+    }
+}
