4974ef2082bbc153c3a1314b3122e4d3533498dd
Sergio Triana <stescobedo.31@gmail.com>
2020-12-08 16:13:01 -0500

    Added cors configurations

--- EmployeeRegistrationAPI/Startup.cs
+++ EmployeeRegistrationAPI/Startup.cs
@@ -28,6 +28,7 @@ namespace EmployeeRegistrationAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors();
 
             var config2 = Configuration.GetConnectionString("DevConnection");
 
@@ -37,6 +38,10 @@ namespace EmployeeRegistrationAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.WithOrigins("http://localhost:3000")
+                .AllowAnyMethod()
+                .AllowAnyOrigin());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
